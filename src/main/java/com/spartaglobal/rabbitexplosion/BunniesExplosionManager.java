cbbe61199edb52e9efe486d92ff164e547f66478
Shmaila Rehman <snrehman16@gmail.com>
2019-10-22 20:02:54 +0100

    Bunnies Manager

---
+++ src/main/java/com/spartaglobal/rabbitexplosion/BunniesExplosionManager.java
@@ -0,0 +1,17 @@
+package com.spartaglobal.rabbitexplosion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BunniesExplosionManager {
+
+    private List<IBunnies> BunnyList = new ArrayList<>();
+    private
+
+     static void intialBunniesPair(){
+        for (int i=0; i < 3; i++){
+
+        }
+    }
+
+}
